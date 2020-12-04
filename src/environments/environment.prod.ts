4703b08fa7587d1daff98bd9e5cf7a03aa5fff1d
Mohamed amine Sekmani <50162422+sekmani@users.noreply.github.com>
2020-12-04 19:51:11 +0100

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyARXWcFqLBbsN7pohKZ8j8a7Xe_asT8ZVI",
+    authDomain: "oneclickdilevryapp.firebaseapp.com",
+    projectId: "oneclickdilevryapp",
+    storageBucket: "oneclickdilevryapp.appspot.com",
+    messagingSenderId: "795137448607",
+    appId: "1:795137448607:web:5fd4768c6461b4c80b60aa",
+    measurementId: "G-D9E3D79503"
+  }
+};
